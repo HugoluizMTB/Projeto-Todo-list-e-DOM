5cef6d54df24af5589600f7817ba732dd8972f89
Hugo Luiz <104656207+HugoluizMTB@users.noreply.github.com>
2022-12-22 15:16:45 -0300

    atualização da ultima aula

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,30 @@
+/**
+ * @param {string} element - nesse parametro é definido o elemento a ser criado 
+ * @param {{}} attrs - Recebe os atributos do elemento a ser criado
+ * @param {} children - 
+ * @example {id: "0", class: "open close"}
+ * @param {string} children - e possivel passar um texto ou um array de elementos criados.
+ * @example createElement("p", null, "Meu Texto") || createElement("div", null, (div, div))
+ */
+function createElement(element, attrs, children) {
+    const elementToCreate = document.createElement(element);
+    if(attrs != null) { 
+        const attrsArray = Object.entries(attrs);
+        for (let i = 0; i <attrsArray.length; i++) {
+            elementToCreate.setAttribute(attrsArray[i][0], attrsArray[i][1]);
+        }
+       }
+       if(typeof children == "string") {
+        elementToCreate.innerText = children;
+       }else {
+        children.forEach((child)=> {
+            elementToCreate.appendChild(child);
+        });
+        
+       }
+       return elementToCreate
+}
+
 const storage = {
     //string
     get(key) {
@@ -8,8 +35,6 @@ const storage = {
     },
 }
 
-export {
-storage
-};
+export {storage, createElement};
 // local storage = permanente
 // local section = permanece na aba até que ela seja fechada
